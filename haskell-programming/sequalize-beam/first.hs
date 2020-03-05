9e40368ddb602eeb12c527c6a42ad7b7d47b2ebf
Vivek Shukla <vivek.shukla@juspay.in>
2020-03-05 12:36:51 +0530

    sequalize-beam

---
+++ haskell-programming/sequalize-beam/first.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.List
+import Data.Map ((!?), (!))
+import qualified Data.Text as T
+import qualified Data.Map as Map
+
+transform :: IO ()
+transform = do
+  str <- readFile "file.txt"
+  putStrLn str
+  let keys = ["/\\", "String", "Int", "where_",":=","==?",",","\"","WHERE"]
+  let values = ["==.","B.val_","B.val_", "\\rec","->", "==.","&&.","","rec"]
+  let keyValueList = zip keys values
+  let mp = Map.fromList keyValueList
+  let list = map (func mp) (words str)
+  putStrLn $ unwords list
+  where
+    func :: Map.Map String String -> String -> String
+    func mp str = case (mp !? str) of
+                    Nothing -> str
+                    Just _ -> (mp ! str)
+
+
+--
+--    extractTableName :: String -> String
+--    extractTableName str = head . tail $ tail  $ dropWhile (/="::") $ words str
